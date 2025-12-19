698a45ce5694cf100e70d6f91d66825e7acd2b89
elikese <elikese@gmail.com>
2025-12-19 20:48:08 +0900

    feat: test1

---
+++ js-study/test/01.js
@@ -0,0 +1,26 @@
+const name = "홍길동";
+const email = "abc@mail.com"
+const follower = ["홍길동"];
+
+// 이름이 "김"으로 시작하면 "김씨입니다" 출력
+// 문자열.startsWith() 사용
+name.startsWith("김") && console.log("김씨입니다.");
+
+// 이메일에 "@" 있으면 "이메일입니다" 출력
+// 문자열.includes() 사용
+email.includes("@") && console.log("이메일 입니다.")
+
+// 팔로워가 존재하면 "팔로워가 존재합니다" 출력
+// 리스트.length 사용
+follower.length && console.log("팔로워가 존재합니다.")
+
+if (name.startsWith("홍")
+    && email.includes("@")
+    && follower.length) {
+    console.log("모든 조건 통과완료!")
+}
+
+name.startsWith("홍")
+    && email.includes("@")
+    && follower.length
+    && console.log("모든 조건 통과완료!")
